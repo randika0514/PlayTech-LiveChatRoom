db2bfc45154c825431f597b79f73c681e809a2f6
randika0514 <randikadimantha@gmail.com>
2022-08-16 11:28:40 +0530

    Updated LoginFormController

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public AnchorPane loginContext;
+    public TextField txtUserName;
+
+    static String userName;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        userName=txtUserName.getText();
+
+        Stage stg = (Stage) txtUserName.getScene().getWindow();
+        stg.close();
+        Stage stg2=new Stage();
+        stg2.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientForm.fxml"))));
+        stg2.setResizable(false);
+        stg2.setTitle("");
+        stg2.centerOnScreen();
+        stg2.show();
+    }
 }
