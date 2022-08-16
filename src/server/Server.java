67dbd3d2dbc2ca95956d196e9a1d3f468c6194ad
randika0514 <randikadimantha@gmail.com>
2022-08-16 10:30:23 +0530

    Server Class Added

---
+++ src/server/Server.java
@@ -0,0 +1,16 @@
+package server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
