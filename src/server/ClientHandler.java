829e56f6721719f016f255a3395f4ffca924ea6c
randika0514 <randikadimantha@gmail.com>
2022-08-16 10:45:14 +0530

    Updated ClientHandler Method

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,4 +1,29 @@
 package server;
 
-public class ClientHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler extends Thread{
+
+    private ArrayList<ClientHandler> clientHandlers;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private PrintWriter printWriter;
+
+    public ClientHandler(Socket socket,ArrayList<ClientHandler> clientHandlers){
+        try {
+            this.socket=socket;
+            this.clientHandlers=clientHandlers;
+            this.bufferedReader= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.printWriter=new PrintWriter(socket.getOutputStream(),true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
